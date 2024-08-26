e4a69d8b5cfaa84a4f8a0806b4398581345d68a3
proguimador1 <guilherme.programmer1@gmail.com>
2024-08-26 19:01:54 -0300

    adição do método da secante

---
+++ metodoSecante.py
@@ -0,0 +1,46 @@
+from math import fabs, log
+
+def f(x):
+    
+    return ... #aqui vai a função
+
+def metodo_iterativo(E,x1,x2):
+    global x
+    
+    for k in range(100000000):
+
+        x3 = x2 - f(x2)/(f(x2) - f(x1)) * (x2-x1)
+
+        if fabs(f(x3)) < E or fabs(x3-x2) < E:
+            x = x3
+            break
+
+        x1 = x2
+        x2 = x3
+
+
+x = None
+
+x1 = ... #aproximação à esquerda
+
+x2 = ... #aproximação à direita
+
+E = ... #precisão desejada na forma de 10^(-n), 
+        #sendo n natural
+
+condicao1 = fabs(f(x1)) < E
+
+condicao2 = fabs(f(x2)) < E or fabs(x2-x1) < E
+
+if condicao1:
+    x = x1
+
+elif condicao2:
+    x = x2
+
+else:
+    metodo_iterativo(E,x1,x2)
+
+p = -1 * log(E,10)
+
+print(f'{x:.{p}f} é um valor válido.')
